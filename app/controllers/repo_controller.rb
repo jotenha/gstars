2440c651272b820141d96c2eedb4e14eaef901d2
Pedro <jotahpeee@live.com>
2024-06-21 12:34:38 -0300

    feat: changed controller to now queue the job instead

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -2,18 +2,26 @@ class RepoController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
 
+
   def create
     username = params[:username]
-    Rails.logger.info "Username: #{username}"
-    github_service = GithubService.new(username)
-    response = github_service.get_repos
-
-    if response.success?
-      user = User.find_or_create_by(name: username)
-      repositories = JSON.parse(response.body).map do |repo|
-        user.repos.create(name: repo['name'], stars: repo['stargazers_count'])
-      end
-    render json: { message: "User and repositories saved successfully" }, status: :ok
-    end
+    Rails.logger.info "Username on controller: #{username}"
+    #GithubJob.perform_async(username)
+    GithubJob.perform_later(username)
+    render json: { message: "Job enqueued" }, status: :ok
   end
 end
+
+
+#old controller
+
+#github_service = GithubService.new(username)
+#    response = github_service.get_repos
+#
+#    if response.success?
+#      user = User.find_or_create_by(name: username)
+#      repositories = JSON.parse(response.body).map do |repo|
+#        user.repos.create(name: repo['name'], stars: repo['stargazers_count'])
+#      end
+#    render json: { message: "User and repositories saved successfully" }, status: :ok
+#    end
