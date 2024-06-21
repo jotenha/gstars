21481278216310026ae4d468c1fe81ddb4690b6a
Pedro <jotahpeee@live.com>
2024-06-20 21:38:38 -0300

    feat: added first iteration of github api interaction

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,4 +1,40 @@
+# app/controllers/RepoController.rb
 class RepoController < ApplicationController
-  def index
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+#require_dependency 'gh_get_service'
+
+  def create
+    username = params[:username]
+    Rails.logger.info "Username: #{username}"
+    github_service = GithubService.new(username)
+    response = github_service.get_repos
+
+    if response.success?
+      repositories = JSON.parse(response.body).map do |repo|
+        {
+          name: repo['name'],
+          stars: repo['stargazers_count']
+        }
+      end
+      render json: repositories, status: :ok
+    else
+      render json: { error: "Failed to fetch repositories for user: #{username}" }, status: :unprocessable_entity
+    end
+  end
+end
+
+class GithubService
+
+  include HTTParty
+
+  base_uri 'https://api.github.com'
+
+  def initialize(username)
+    @username = username
+  end
+
+  def get_repos
+    self.class.get("/users/#{@username}/repos")
   end
 end
