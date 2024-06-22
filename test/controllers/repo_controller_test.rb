53276121f757b05653526d3c50ab47a5e0ef782c
Pedro <jotahpeee@live.com>
2024-06-22 16:04:26 -0300

    feat: added basic test for controller

--- test/controllers/repo_controller_test.rb
+++ test/controllers/repo_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class RepoControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get repo_index_url
-    assert_response :success
-  end
+test "should create job and return success" do
+  post repo_create_url, params: { username: "test_user" }
+  assert_response :success
+  assert_equal "Job enqueued", response.parsed_body["message"]
 end
