25ec0ef12abbc3f4272d33da8a3e497924913b99
Pedro <jotahpeee@live.com>
2024-06-22 17:38:08 -0300

    test: user controller can now search for single user

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -7,4 +7,13 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     assert_equal response.content_type, 'application/json'
     assert_not_empty response.body
   end
+
+  test "should get user" do
+    username = 'test_user'
+    get get_user_path(username: username)
+    assert_response :success
+    assert_equal response.content_type, 'application/json'
+    assert_not_empty response.body
+  end
+
 end
