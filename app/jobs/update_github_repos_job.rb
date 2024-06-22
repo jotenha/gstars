a7b26772e05348f88ab66c6786710ae544cd3974
Pedro <jotahpeee@live.com>
2024-06-22 18:48:11 -0300

    refactor: added logging

--- app/jobs/update_github_repos_job.rb
+++ app/jobs/update_github_repos_job.rb
@@ -1,11 +1,13 @@
 class UpdateGithubReposJob < ApplicationJob
   queue_as :default
 
-  #github probably has a rate limit but just for the ideia of the code
+  #github probably has a rate limit but just for the ideia of "keeping track"
 
   def perform
     User.find_each do |user|
       GithubJob.perform_later(user.name)
+      Rails.logger.info "Finished updating user #{user.name}"
     end
+    Rails.logger.info "Finished UpdateGithubReposJob at #{Time.current}"
   end
 end
