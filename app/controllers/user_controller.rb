7445811436db1661fe19ba1650b65d17d83b659b
Pedro <jotahpeee@live.com>
2024-06-22 17:31:45 -0300

    feat: added controller for user/repo browsing

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UserController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:get_users]
+
+  def get_users
+    users = User.all.map do |user|
+      { name: user.name, repos: user.repos.map{|repo| { name: repo.name, stars: repo.stars}}}
+    end
+    render json: users, status: :ok
+  end
