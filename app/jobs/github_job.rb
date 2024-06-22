8d9466ab849d903e6952e702304c63a945b37804
Pedro <jotahpeee@live.com>
2024-06-22 19:29:06 -0300

    feat: github job now properly handles updates and doesn't add dupes

--- app/jobs/github_job.rb
+++ app/jobs/github_job.rb
@@ -1,24 +1,26 @@
 class GithubJob < ApplicationJob
-  #taks priority?
   queue_as :default
-
-  #in case of per-job backend
-  #self.queue_adapter = :sidekiq
 
   def perform(username)
     Rails.logger.info "Username on job: #{username}"
     github_service = GithubService.new(username)
     response = github_service.get_repos
-     if response.success?
-          #if status is 200
-          user = User.find_or_create_by(name: username)
-          repositories = JSON.parse(response.body).map do |repo|
-            #probably check if the repo is already there
-            user.repos.create(name: repo['name'], stars: repo['stargazers_count'])
+    if response.success?
+      user = User.find_or_create_by(name: username)
+      JSON.parse(response.body).each do |repo|
+        existing_repo = user.repos.find_by(name: repo['name'])
+        if existing_repo
+          # Update stars count if it has changed
+          #Rails.logger.info "Repo found: #{repo['name']}..trying to update!"
+          if existing_repo.stars != repo['stargazers_count']
+            existing_repo.update(stars: repo['stargazers_count'])
+            #Rails.logger.info "Repo changed: #{repo['name']}..updated!"
           end
-        #status is 404, 422, etc
         else
-          Rails.logger.error "Error on job: #{response.body.message}"
+          # Create a new repo if it doesn't exist
+          user.repos.create(name: repo['name'], stars: repo['stargazers_count'])
         end
-end
+      end
+    end
+  end
 end
