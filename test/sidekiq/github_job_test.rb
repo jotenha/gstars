9639e681ae88d786132a05b5e85e2380127c9738
Pedro <jotahpeee@live.com>
2024-06-22 16:05:36 -0300

    feat: added basic test for github job

---
+++ test/sidekiq/github_job_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+class GithubJobTest < Minitest::Test
+  def test_example
+    skip "add some examples to (or delete) #{__FILE__}"
+  end
+end
+class GithubJobTest < Minitest::Test
+  def setup
+    @username = 'test_user'
+    @job = GithubJob.new
+  end
+
+  def test_perform
+    # stub GithubService
+    github_service = Minitest::Mock.new
+    GithubService.stub(:new, github_service) do
+      # stub response
+      response = OpenStruct.new(success?: true, body: '[{"name": "repo1", "stargazers_count": 10}, {"name": "repo2", "stargazers_count": 20}]')
+      github_service.expect(:get_repos, response)
+
+      # stub User
+      user = Minitest::Mock.new
+      User.stub(:find_or_create_by, user) do
+        # stub Repos
+        repos = Minitest::Mock.new
+        user.expect(:repos, repos)
+        repos.expect(:create, nil, [{ name: 'repo1', stars: 10 }])
+        repos.expect(:create, nil, [{ name: 'repo2', stars: 20 }])
+
+        @job.perform(@username)
+      end
+    end
+    assert_mock github_service
+    assert_mock user
+    assert_mock repos
+  end
+end
