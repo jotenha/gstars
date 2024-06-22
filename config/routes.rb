f3a099efe110d58c0b1f8aaef4331f0c735236a1
Pedro <jotahpeee@live.com>
2024-06-22 18:03:43 -0300

    feat: added remove user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'get_users', to: 'user#get_users'
   get 'get_users/:username', to: 'user#get_user'
   post 'saverepos', to: 'repo#create'
+  delete 'remove_user/:username', to: 'user#remove'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
