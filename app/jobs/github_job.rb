b04fe8449f87183a7c35fd2d8b0e3c02fe2a1d55
Pedro <jotahpeee@live.com>
2024-06-22 23:13:52 -0300

    refactor:  changes to fetch db a lot less times minimizing quieries

--- app/jobs/github_job.rb
+++ app/jobs/github_job.rb
@@ -5,19 +5,21 @@ class GithubJob < ApplicationJob
     Rails.logger.info "Username on job: #{username}"
     github_service = GithubService.new(username)
     response = github_service.get_repos
+
     if response.success?
       user = User.find_or_create_by(name: username)
-      JSON.parse(response.body).each do |repo|
-        existing_repo = user.repos.find_by(name: repo['name'])
+      repos = JSON.parse(response.body)
+
+      existing_repos = user.repos.where(name: repos.map { |repo| repo['name'] }).index_by(&:name)
+
+      repos.each do |repo|
+        existing_repo = existing_repos[repo['name']]
         if existing_repo
           # Update stars count if it has changed
-          #Rails.logger.info "Repo found: #{repo['name']}..trying to update!"
           if existing_repo.stars != repo['stargazers_count']
             existing_repo.update(stars: repo['stargazers_count'])
-            #Rails.logger.info "Repo changed: #{repo['name']}..updated!"
           end
         else
-          # Create a new repo if it doesn't exist
           user.repos.create(name: repo['name'], stars: repo['stargazers_count'])
         end
       end
